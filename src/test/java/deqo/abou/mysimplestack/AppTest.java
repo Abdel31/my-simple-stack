a64afc7d37c2b11138d38059720aa8dd08cd705e
Abdel31 <bouras.abdelhadi@hotmail.fr>
2018-11-05 17:35:58 +0100

    #1 testApp

--- src/test/java/deqo/abou/mysimplestack/AppTest.java
+++ src/test/java/deqo/abou/mysimplestack/AppTest.java
@@ -7,12 +7,8 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
 
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+ /*ok*/
 }
